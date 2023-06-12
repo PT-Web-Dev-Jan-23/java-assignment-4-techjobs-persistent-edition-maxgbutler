4d28961f14e8198714aeb6d4ea932fde345c088c
Max Butler <maxgbutler003@gmail.com>
2023-06-12 16:58:58 -0500

    updated to process creation of employers and skills.

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,61 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "All Skills");
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
+    }
+
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute("title", "Add Skill");
+        model.addAttribute("skill", new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Skill");
+            return "skills/add";
+        }
+
+        skillRepository.save(newSkill);
+        return ("redirect:/skills");
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(@PathVariable Integer skillId, Model model) {
+        Optional<Skill> optSkill = skillRepository.findById(skillId);
+        if (optSkill.isEmpty()) {
+            model.addAttribute("title", "Invalid skill ID:" + skillId);
+            return "skills/view";
+        }
+
+        Skill skill = (Skill) optSkill.get();
+        model.addAttribute("skill", skill);
+        return "skills/view";
+    }
+}
