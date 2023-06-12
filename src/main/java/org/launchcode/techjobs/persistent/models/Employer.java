5c7c92b5a2a298ce525e6aec6a02ad213febbf3a
Max Butler <maxgbutler003@gmail.com>
2023-06-12 16:57:59 -0500

    Created many to many relationship between Job and Skill, and one to many relationship between Employer and Job.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank
     @Size(min = 3, max = 99, message = "name must be between 3 & 99 characters")
     private String location;
+
+    @OneToMany(mappedBy = "employer")
+    //@JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(max=500, message="Description must be less than 500 characters.")
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -17,6 +23,14 @@ public class Skill extends AbstractEntity {
 
     public String getDescription() {
         return description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJob(Job job) {
+        this.jobs.add(job);
     }
 
     public void setDescription(String description) {
